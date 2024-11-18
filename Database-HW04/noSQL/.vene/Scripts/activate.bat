ebf86fa384dfad0b874e7249733be175cf5667a5
PoyuTseng <poyutseng781217@gmail.com>
2024-11-18 11:44:51 +0800

    DATABASEHW04

--- Database-HW04/noSQL/.vene/Scripts/activate.bat
+++ Database-HW04/noSQL/.vene/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\pecu6\OneDrive\文件\GitHub\Database\noSQL\.vene
+set VIRTUAL_ENV=D:\1131DB\1131Database\Database-HW04\noSQL\.vene
 
 if not defined PROMPT set PROMPT=$P$G
 
